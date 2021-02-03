07ec370e1a8ed13c9c8f530958691f7c81358090
votraorllatii <vo36043@ubt-uni.net>
2021-02-03 15:32:09 +0100

    Update

--- clothingapp/src/components/cart-icon/cart-icon.component.jsx
+++ clothingapp/src/components/cart-icon/cart-icon.component.jsx
@@ -5,13 +5,13 @@ import { createStructuredSelector } from 'reselect';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+// import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
-    <ShoppingIcon className='shopping-icon' />
+    {/* <ShoppingIcon className='shopping-icon' /> */}
     <span className='item-count'>{itemCount}</span>
   </div>
 );
--- clothingapp/src/components/header/header.component.jsx
+++ clothingapp/src/components/header/header.component.jsx
@@ -9,14 +9,14 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { ReactComponent as Crown }  from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
-      <Logo className='logo' />
+      <Crown className='logo'  />
     </Link>
     <div className='options'>
       <Link className='option' to='/shop'>
